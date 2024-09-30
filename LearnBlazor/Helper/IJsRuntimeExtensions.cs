f3eabbf87f31d7ac286bceeb5a7d9c723dd2a3cd
Karin Åstrand <karinastrand@yahoo.se>
2024-09-30 19:35:46 +0200

    backup

---
+++ LearnBlazor/Helper/IJsRuntimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+namespace LearnBlazor.Helper;
+
+public static class IJsRuntimeExtensions
+{
+    public static async Task ToastrSuccess(this IJSRuntime jsRuntime,string message)
+    {
+        await jsRuntime.InvokeVoidAsync("showToastr","success", message);
+    }
+    public static async Task ToastrError(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("showToastr", "error", message);
+    }
+}
---
+++ LearnBlazor/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace LearnBlazor.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public bool IsActive { get; set; }
+    public double Price { get; set; }
+    public IEnumerable<Product_Prop> ProductProperties { get; set; }=new List<Product_Prop>();
+}
